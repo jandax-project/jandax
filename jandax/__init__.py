e80257b12d3453f23c4b4dde7bc74099604cc80c
Vivek Menon <vivek@vmbair.attlocal.net>
2025-03-03 19:58:40 -0600

    update init to register pytree

--- jandax/__init__.py
+++ jandax/__init__.py
@@ -3,7 +3,11 @@ Jandax - Traceable and Portable DataFrames for C++ Integration
 """
 
 # Import core components first
+# Import pytree registrations (this executes the registrations)
+# This must happen after the classes are imported but before any instances are created
 from jandax.core import DataFrame, GroupBy, GroupByRolling, Rolling
+
+import jandax.pytree  # isort:skip
 
 # Version information
 __version__ = "0.1.0"
